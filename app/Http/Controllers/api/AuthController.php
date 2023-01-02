e46560079e11f22a622d98841c4166a3f8d652b9
Rusdy Masykuri <rusdy.begundal@gmail.com>
2023-01-02 10:21:10 +0700

    Authentication Controller added for login function

---
+++ app/Http/Controllers/api/AuthController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Auth;
+use App\Models\User;
+
+class AuthController extends Controller
+{
+    //Login API
+
+    public function login(Request $request)
+    {
+        if (!Auth::attempt($request->only('email', 'password')))
+        {
+            return response()
+                ->json(['message' => 'Unauthorized'], 401);
+        }
+
+        $user = User::where('email', $request['email'])->firstOrFail();
+
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()
+            ->json(['message' => 'Hi '.$user->name.', welcome to home','access_token' => $token, 'token_type' => 'Bearer', ]);
+    }
+}

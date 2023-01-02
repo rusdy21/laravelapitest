bcec36052bef066e9692bacf2b730a7e1f7c0f4b
Rusdy Masykuri <rusdy.begundal@gmail.com>
2023-01-02 08:33:50 +0700

    change default route and added seeder user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,18 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        $user = [[
+
+
+            'name'=> 'gw banget',
+            'email'=>'rusdy.begundal@gmail.com',
+            'password'=> bcrypt('123456')
+
+        ]];
+
+        foreach($user as $key=>  $value){
+        User::create($value);
+        }
     }
 }
